9ae0104987683004a6a7d15db30e01a1b50829ae
Sarahwcode <sarahw181090@gmail.com>
2024-12-09 20:03:09 +0000

    changes to contact me page

--- index.js
+++ index.js
@@ -1,8 +1,1 @@
-function soundResponse(event) {
-  event.preventDefault();
-  let inputValue = document.querySelector("#sound-bath-input");
-  console.log(inputValue.value);
-}
-
-let submitSound = document.querySelector("#sound-bath");
-submitSound.addEventListener("submit", soundResponse);
+alert("hello");
