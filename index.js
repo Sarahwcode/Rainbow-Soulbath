6896c35812acc73118b41ab9541dbd937b6c95f9
Sarahwcode <sarahw181090@gmail.com>
2024-12-10 14:34:31 +0000

    added javascript on contact me page

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-alert("hello");
+//function peaceWord(event) {
+//event.preventDefault();
+//let formSubmit = document.querySelector("#form-text");
+//console.log(formSubmit.value);
+//}
+//let formText = document.querySelector("#form");
+//formText.addEventListener("submit", peaceWord);
+
+//document.getElementById("form").addEventListener("submit", function (event) {
+//event.preventDefault(); // Prevent the default form submission behavior
+//const peacefulWord = document.getElementById("form-text").value;
+//console.log("The word that brings you peace is:", peacefulWord);
+//});
