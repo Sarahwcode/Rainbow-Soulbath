513991cae9b4612cf55e04255417e4e934843662
Sarahwcode <sarahw181090@gmail.com>
2025-03-04 19:39:45 +0000

    php changes

--- test_post.php
+++ test_post.php
@@ -1,6 +1,6 @@
 <?php
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $to = "sarah_ward90@live.com"; // Replace with your email address
+    $to = "admin@rainbowsoulbathfamily.co.uk"; // Replace with your email address
     $subject = "Form Data from test_email_post.php";
     $message = "Form Data:\n\n" . print_r($_POST, true); // Get the $_POST array as a string
     $headers = "From: webserver@localhost\r\n" .
